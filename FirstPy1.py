5cd4ae4a3aa4e9922c6500d0723d24f206a7c82e
hongxu123 <60809967+hongxu123@users.noreply.github.com>
2020-05-16 20:07:11 +0800

    wxPython第一次修改
    添加静态文本。

--- FirstPy1.py
+++ FirstPy1.py
@@ -3,7 +3,8 @@ import wx
 class MyFrame(wx.Frame):
   def __init__(self,parent):
     frame = wx.Frame.__init__(self, parent, title="用户登录系统", pos=(100,100), size=(300,300))
-  
+    panel = wx.Panel(self)
+    titel = wx.StaticText(panel, label = "wxPython之禅"， pop=(100,200))
   
 if __name__ == "__main__":
   app = wx.App()
